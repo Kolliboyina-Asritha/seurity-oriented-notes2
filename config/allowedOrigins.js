b225a16f6425234d5f64a43ffcf3afae9ed5269c
Kolliboyina-Asritha <asrithaasritha99@gmail.com>
2026-05-27 14:57:37 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,2 +1,2 @@
-const allowedOrigins=['http://localhost:8080','http://127.0.0.1:5500']
+const allowedOrigins=['http://localhost:8080','http://127.0.0.1:5500','https://security-oriented-notes.netlify.app']
 module.exports=allowedOrigins;
